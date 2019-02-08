d4bd118641d6965a0e296c9081f6013715a28680
Prashaath R <arashpath@gmail.com>
2019-02-09 02:31:03 +0530

    backto 7.5

--- download.sh
+++ download.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-base='7.6.1810'                                           # Host CentOS Version
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+image=centos ; ver=7.5.1804                                         # Host CentOS Version
+dir="$(dirname $(readlink -f $0))"
 case "$1" in
 
   centos) # Official CentOS Docker Image -------------------------------------#
-    centos_version="CentOS-$base"
-    sig_repo="https://github.com/CentOS/sig-cloud-instance-images"
-    image_url=$sig_repo/raw/$centos_version/docker
-    curl -o centos/centos-7-docker.tar.xz $image_url/centos-7-docker.tar.xz
-    curl -o centos/scratch.Dockerfile $image_url/Dockerfile
+    
+    docker image pull $image:$ver
+    docker image tag $image:$ver fssai/$image:$ver
+    docker save fssai/$image:$ver | pxz > $image-$ver.tar.xz
+    pxz -cd $image-$ver.tar.xz | docker load
     ;;
   
-  docker) # Docker RPMs for offline installation -----------------------------#
-    centos_image="centos:$base"
+  docker|nginx|node) # Docker RPMs for offline installation -----------------------------#
+    centos_image="$image:$ver"
     docker run --rm \
       -v $dir/rpm:/rpm \
       $centos_image sh /rpm/get_rpm.sh $1
--- rpm/get_rpm.sh
+++ rpm/get_rpm.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-rpmdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+rpmdir="$(dirname $(readlink -f $0))"
+yumdir=/var/cache/yum/x86_64/7
 function tar_rpm {
-  tar -C /var/cache/yum/x86_64/7/$1 -cJvf "$rpmdir/$1".tar.xz packages
+  tar -C $yumdir/$1 -cJvf "$rpmdir/$1".tar.xz packages
 }
+
+read -r -d '' NGINX_REPO << EOM
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+gpgcheck=1
+enabled=1                 
+gpgkey=https://nginx.org/keys/nginx_signing.key               
+EOM
+
+
 
 case $1 in
 
@@ -13,6 +25,23 @@ case $1 in
     tar_rpm docker-ce-stable
     ;;
 
+  nginx)
+    echo "$NGINX_REPO" > /etc/yum.repos.d/nginx.repo  
+    yum install -y nginx --downloadonly
+    mv $yumdir/nginx-stable/packages/*.rpm $rpmdir
+    ;;
+  
+  node)
+    curl -sL https://rpm.nodesource.com/setup_10.x | bash -
+    curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+    yum -y install nodejs yarn --downloadonly
+    mv $yumdir/nodesource/packages/*.rpm $rpmdir
+    mv $yumdir/yarn/packages/*.rpm $rpmdir
+    ;;
+  
+  postgres)
+    #yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
+    ;;
   *) 
     echo "Invalid option"
     ;;
